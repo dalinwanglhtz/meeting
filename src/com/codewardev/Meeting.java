0be370894ad6871d4f23c7092b543c3ebc6c3a5b
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-14 23:09:27 +1100

    started

---
+++ src/com/codewardev/Meeting.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/59df2f8f08c6cec835000012/train/java
+
+public class Meeting {
+
+	public static String meeting(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
