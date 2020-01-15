1b406e30e7b9bea1c24367ee8eaa872b1a2c303d
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-15 21:42:48 +1100

    first working cut

--- src/com/codewardev/Meeting.java
+++ src/com/codewardev/Meeting.java
@@ -1,12 +1,24 @@
 package com.codewardev;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 // https://www.codewars.com/kata/59df2f8f08c6cec835000012/train/java
 
 public class Meeting {
 
 	public static String meeting(String s) {
-		// TODO Auto-generated method stub
-		return null;
+		return Arrays.stream(s.split(";")).map(x->{
+			String[] name = x.split(":");
+			return String.join(":", name[1], name[0]);
+		})
+		.map(String::toUpperCase)
+		.sorted()
+		.map(x->{
+			String[] name = x.split(":");
+			return "("+name[0]+", "+name[1]+")";
+		})
+		.collect(Collectors.joining());
 	}
 
 }
